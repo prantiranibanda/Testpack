4dc0a821b551649ddb440e5fd62434895593036b
pranti <prantibanda@gmail.com>
2026-03-08 17:16:03 +0530

    Export functions

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const isW = require("is-wds");
+
 function scream() {
 	console.log("Faahh");
 	return "Faahh";
@@ -6,3 +8,9 @@ function scream() {
 function checkFaah(str) {
 	return str === "Faahh";
 }
+
+function checkWds(str) {
+	return isW(str);
+}
+
+module.exports = { scr: scream, checkFaah, checkWds };
